7a158247985147121bc3a3996d3afd338515db6b
asadlion21 <70730782+asadlion21@users.noreply.github.com>
2021-01-14 18:43:46 -0500

    Update app.js

--- ufo-level-1/StarterCode/static/js/app.js
+++ ufo-level-1/StarterCode/static/js/app.js
@@ -1,5 +1,42 @@
 // from data.js
 var tableData = data;
 
-console.log(tableData);
+//console.log(tableData);
 // YOUR CODE HERE!
+
+//creating references
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFeildDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "comments"]
+
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings =>{
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+
+}
+
+addData(tableData);
+
+//creating event listener for the button
+//setting up filter button 
+
+button.on("click", () => {
+    d3.event.preventDefault();
+
+    var inputDate = inputFeildDate.property("value").trim();
+    //console.log(inputDate)
+    
+
+
+
+
+}
+
+
+
+)
